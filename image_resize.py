47cc44873653809abd67fac43e392c54711ed9eb
igorzakhar <igorzakhar@gmail.com>
2017-05-24 17:18:18 +0300

    Added resize_image implementation

--- image_resize.py
+++ image_resize.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 from PIL import Image
@@ -13,6 +14,8 @@ def process_args():
                         help="Input value heigth in px")
     parser.add_argument('-s', '--scale', type=float, 
                         default=None, help="Input value scale")
+    parser.add_argument('-p', '--path', default=os.getcwd(),
+                        help="Set the directory paths for saving file ")
     return parser.parse_args()
 
 
@@ -41,9 +44,30 @@ def get_resize_value(original_size, width=None, heigth=None, scale=None):
     return (round(x_resize), round(y_resize))
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def resize_image(path_to_original, path_to_result, width, heigth, scale):
+    path_to_original = os.path.abspath(path_to_original.name)
+    path_to_result = os.path.abspath(path_to_result)
+    if not os.path.exists(path_to_result):
+        os.mkdir(path_to_result)
+    try:
+        with Image.open(path_to_original) as image:
+            xsize, ysize = image.size
+            x_resize, y_resize = get_resize_value(image.size, 
+                                                  width, 
+                                                  heigth,
+                                                  scale)
+            
+            filename = os.path.basename(image.filename).rsplit('.', 1)[0]
+            new_filename = "{}_{}x{}.{}".format(filename,
+                                                x_resize, 
+                                                y_resize,
+                                                image.format.lower())
+            image = image.resize((x_resize, y_resize), Image.ANTIALIAS)
+            image.save(os.path.join(path_to_result, new_filename))
+    except IOError:
+        print("File does not contain image data")
 
 
 if __name__ == '__main__':
-    pass
+    args = process_args()
+    resize_image(args.file, args.path, args.width, args.heigth, args.scale)
