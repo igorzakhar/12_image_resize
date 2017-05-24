34d9cef8a1d7f013c7d940fe5121ddfa3f4fcc37
igorzakhar <igorzakhar@gmail.com>
2017-05-24 11:40:25 +0300

    Added process_args function

--- image_resize.py
+++ image_resize.py
@@ -1,3 +1,19 @@
+import argparse
+
+from PIL import Image
+
+
+def process_args():
+    parser = argparse.ArgumentParser(description="Resize image from file")
+    parser.add_argument('file', type=argparse.FileType('r'), 
+                        help="Input filename")
+    parser.add_argument('-x', '--width', type=float, default=None, 
+                        help="Input value width in px")
+    parser.add_argument('-y', '--heigth', type=float, default=None, 
+                        help="Input value heigth in px")
+    parser.add_argument('-s', '--scale', type=float, 
+                        default=None, help="Input value scale")
+    return parser.parse_args()
 
 
 def resize_image(path_to_original, path_to_result):
