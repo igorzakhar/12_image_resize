f78c3eaf478971733cb0bd82a57411fb170ce325
igorzakhar <igorzakhar@gmail.com>
2017-05-24 11:47:58 +0300

    Added get_resize_value function

--- image_resize.py
+++ image_resize.py
@@ -16,6 +16,31 @@ def process_args():
     return parser.parse_args()
 
 
+def get_resize_value(original_size, width=None, heigth=None, scale=None):
+    x_size, y_size = original_size
+    ratio = None
+    x_resize = None
+    y_resize = None
+    
+    if width and heigth:
+        x_resize = width
+        y_resize = heigth
+    elif width:
+        ratio = x_size / width
+        print(ratio)
+        x_resize = width
+        y_resize = y_size / ratio
+    elif heigth:
+        ratio = y_size / heigth
+        x_resize = x_size / ratio
+        y_resize = heigth
+    elif scale:
+        x_resize = x_size * scale
+        y_resize = y_size * scale
+
+    return (round(x_resize), round(y_resize))
+
+
 def resize_image(path_to_original, path_to_result):
     pass
 
