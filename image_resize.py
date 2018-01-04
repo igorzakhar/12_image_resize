8109376ef2cd9cb01abce5790cdd97a7bb7f3d70
igorzakhar <igorzakhar@gmail.com>
2018-01-04 17:42:09 +0300

    Transfer code from if main to main() function

--- image_resize.py
+++ image_resize.py
@@ -6,16 +6,38 @@ from PIL import Image
 
 def process_args():
     parser = argparse.ArgumentParser(description="Resize image from file")
-    parser.add_argument('file', type=argparse.FileType('r'), 
-                        help="Input filename")
-    parser.add_argument('-x', '--width', type=float, default=None, 
-                        help="Input value width in px")
-    parser.add_argument('-y', '--heigth', type=float, default=None, 
-                        help="Input value heigth in px")
-    parser.add_argument('-s', '--scale', type=float, 
-                        default=None, help="Input value scale")
-    parser.add_argument('-p', '--path', default=os.getcwd(),
-                        help="Set the directory paths for saving file ")
+    parser.add_argument(
+        'file',
+        type=argparse.FileType('r'),
+        help='Input filename'
+    )
+    parser.add_argument(
+        '-x',
+        '--width',
+        type=float,
+        default=None,
+        help='Input value width in px'
+    )
+    parser.add_argument(
+        '-y',
+        '--heigth',
+        type=float,
+        default=None,
+        help='Input value heigth in px'
+    )
+    parser.add_argument(
+        '-s',
+        '--scale',
+        type=float,
+        default=None,
+        help='Input value scale'
+    )
+    parser.add_argument(
+        '-p',
+        '--path',
+        default=os.getcwd(),
+        help='Set the directory paths for saving file '
+    )
     return parser.parse_args()
 
 
@@ -33,7 +55,7 @@ def get_resize_value(original_size, width=None, heigth=None, scale=None):
     ratio = None
     x_resize = None
     y_resize = None
-    
+
     if width and heigth:
         x_resize = width
         y_resize = heigth
@@ -60,22 +82,31 @@ def resize_image(image_data, path_to_result, x_resize, y_resize):
         os.mkdir(path_to_result)
     xsize, ysize = image_data.size
     filename = os.path.basename(image_data.filename).rsplit('.', 1)[0]
-    new_filename = "{}_{}x{}.{}".format(filename,
-                                        x_resize, 
-                                        y_resize,
-                                        image_data.format.lower())
-    
+    new_filename = '{}_{}x{}.{}'.format(
+        filename,
+        x_resize,
+        y_resize,
+        image_data.format.lower()
+    )
     image_new = image_data.resize((x_resize, y_resize), Image.ANTIALIAS)
     image_new.save(os.path.join(path_to_result, new_filename))
     print('File {} has been saved'.format(new_filename))
 
 
-if __name__ == '__main__':
+def main():
     args = process_args()
     image_data = get_image_data(args.file)
     if image_data:
-        x_resize, y_resize = get_resize_value(image_data.size, args.width, 
-                                              args.heigth, args.scale)
+        x_resize, y_resize = get_resize_value(
+            image_data.size,
+            args.width,
+            args.heigth,
+            args.scale
+        )
         resize_image(image_data, args.path, x_resize, y_resize)
     else:
         print("File does not contain image data")
+
+
+if __name__ == '__main__':
+    main()
